26c196e0c8f96049aa3a301e6a969650652ed9b9
reraparius <80336019+reraparius@users.noreply.github.com>
2021-04-24 19:28:58 +0700

    minggu_7

---
+++ C.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_B;
+
+/**
+ *
+ * @author reraparius
+ */
+public class C extends B {
+     void callthis(){
+        System.out.println("Inside Class C's Method!");
+    }
+}
+
---
+++ Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pewarisan;
+
+/**
+ *
+ * @author reraparius
+ */
+public class Test {
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("Rera Varius Chindua", 158, 50, "E41200170", "POLITEKNIK NEGERI JEMBER", 83);
+        System.out.println(mahasiswa.toString());
+    }
+}
+
